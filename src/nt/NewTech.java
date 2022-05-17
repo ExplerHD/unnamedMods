4ae55d598e0b66f4b0f51c75d2d65e872f859454
ExplerHD <73381264+ExplerHD@users.noreply.github.com>
2022-05-17 21:59:16 +0700

    Update and rename src/example/ExampleJavaMod.java to src/nt/NewTech.java

---
+++ src/nt/NewTech.java
@@ -0,0 +1,27 @@
+package example;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class NewTech extends Mod{
+
+    public NewTech(){
+        Log.info("Loaded NewTech constructor.");
+    }
+
+    @Override
+    public void loadContent(){
+//        NTStatus.load();
+//        NTItemLiquid.load();
+//        NTBullets.load();
+//        NTUnits.load();
+//        NTBlocks.load();
+    }
+
+}
